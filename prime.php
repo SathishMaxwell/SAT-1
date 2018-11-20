91b123557af24716807a19ef4661a913db2f1632
SathishMaxwell <45192185+SathishMaxwell@users.noreply.github.com>
2018-11-20 11:49:06 +0530

    Update prime.php

--- prime.php
+++ prime.php
@@ -1,4 +1,38 @@
 <html>
 <body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="number" name="fname">
+  <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $count=0;
+    $name = $_POST['fname'];
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+       for($i=1;$i<=$name;$i++)
+       {
+           $t=$name%$i;
+           if($t==0)
+           {
+               $count+=1;
+           }
+       }
+       if($count==2)
+       {
+       echo"$name prime";
+       }
+       else
+       {
+       echo "$name not prime";
+       } 
+   }
+}
+?>
+
 </body>
 </html>
